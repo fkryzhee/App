e9be0a32bcf35093b8261b877a5fc253f92893c1
fkryzhee <fikrisidiq22@gmail.com>
2023-10-16 06:35:10 +0700

    updatemainprogram

--- App.py
+++ App.py
@@ -1,14 +1,19 @@
 import streamlit as st
 import csv
+import pandas as pd
 
-# Step 1: Define Kontak struct
+# 1. Buatlah sebuah tipe data terstruktur (struct) yang disebut Kontak yang memiliki atribut-atribut berikut:
+# Nama kontak (string)
+# Alamat email kontak (string)
+# No telepon kontak (string)
+
 class Kontak:
     def __init__(self, nama, email, telepon):
         self.nama = nama
         self.email = email
         self.telepon = telepon
 
-# Step 2: Load existing data or create an empty list
+# 2. array untuk menyimpan kontak-kontak
 def load_data():
     try:
         with open('kontak.csv', mode='r') as file:
@@ -24,7 +29,7 @@ def save_data(daftar_kontak):
         for kontak in daftar_kontak:
             writer.writerow([kontak.nama, kontak.email, kontak.telepon])
 
-# Step 3: Define functions for each operation
+# 3. Fungsi tambah_kontak yang memungkinkan pengguna untuk memasukkan data kontak baru ke dalam program.
 def tambah_kontak():
     nama = st.text_input("Nama:")
     email = st.text_input("Email:")
@@ -36,6 +41,7 @@ def tambah_kontak():
         save_data(daftar_kontak)
         st.success("Kontak berhasil ditambahkan!")
 
+# 4. Fungsi hapus_kontak yang memungkinkan pengguna untuk menghapus kontak berdasarkan nama kontak.
 def hapus_kontak():
     nama_hapus = st.text_input("Nama Kontak yang akan dihapus:")
     if st.button("Hapus Kontak"):
@@ -48,14 +54,18 @@ def hapus_kontak():
         else:
             st.warning(f"Tidak ada kontak dengan nama {nama_hapus}.")
 
+# 6. Fungsi tampilkan_daftar_kontak yang menampilkan daftar semua kontak yang ada, termasuk nama, nomor telepon, dan alamat email.
 def tampilkan_daftar_kontak():
     st.header("Daftar Kontak")
-    for kontak in daftar_kontak:
-        st.write(f"Nama: {kontak.nama}")
-        st.write(f"Email: {kontak.email}")
-        st.write(f"Nomor Telepon: {kontak.telepon}")
-        st.write("-" * 20)
+    data = [[i+1] + [kontak.nama, kontak.email, kontak.telepon] for i, kontak in enumerate(daftar_kontak)]
+    if data:
+        df = pd.DataFrame(data, columns=["No", "Nama", "Email", "Nomor Telepon"])
+        df = df.set_index("No")  # Menjadikan kolom "No" sebagai indeks
+        st.table(df)
+    else:
+        st.write("Tidak ada kontak yang tersedia.")
 
+# 7. Fungsi cari_kontak yang memungkinkan pengguna untuk mencari kontak berdasarkan nama.
 def cari_kontak():
     nama_cari = st.text_input("Cari Nama Kontak:")
     if st.button("Cari Kontak"):
@@ -71,7 +81,7 @@ def cari_kontak():
         if not ditemukan:
             st.warning(f"Tidak ada kontak dengan nama {nama_cari}.")
 
-# Step 4: Main program
+# 7. Program utama yang memberikan menu kepada pengguna untuk melakukan operasi-operasi yang telah dibuat.
 def main():
     st.title("Aplikasi Pengelola Kontak")
     
